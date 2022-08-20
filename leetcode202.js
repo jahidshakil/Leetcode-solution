a8ade7295f58b9a6437dda8a3560571f590a5cf4
jahidshakil <jahidaladin@gmail.com>
2022-08-20 11:29:53 +0600

    Happy Number

---
+++ leetcode202.js
@@ -0,0 +1,26 @@
+/* Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+*/
+
+
+var isHappy = function (n) {
+
+    let sum =0
+    while (n > 0) {
+    let e = n % 10;
+    n = Math.floor(n / 10);
+    sum += e * e;
+  }
+    
+    if(sum ===1) return true
+    if(sum>1 && sum<=4) return false
+    
+    
+    return isHappy(sum)
+};
