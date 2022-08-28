2354e4db82ae82a0569d2a111c93bdad92c875ab
jahidshakil <jahidaladin@gmail.com>
2022-08-28 23:28:23 +0600

    Maximum Product of Three Numbers

---
+++ leetcode628.js
@@ -0,0 +1,18 @@
+/*
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+*/
+
+
+var maximumProduct = function(nums) {
+    
+    nums=nums.sort((a,b)=>a-b);
+    let n=nums.length;
+    
+    let p = nums[n-3]*nums[n-2]*nums[n-1];
+    
+    let q= nums[0]*nums[1]*nums[n-1];
+    
+    return Math.max(p,q);
+
+};
