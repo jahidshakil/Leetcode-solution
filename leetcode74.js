54e957b6cd55224850ecd515e4b5f21a27336714
jahidshakil <jahidaladin@gmail.com>
2022-09-06 03:59:43 +0600

    Search in 2D matrix

---
+++ leetcode74.js
@@ -0,0 +1,26 @@
+/*
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+ 
+*/
+
+
+var searchMatrix = function(matrix, target) {
+    let r = 0;
+    let c = matrix[0].length-1;
+
+    while(r < matrix.length && c >= 0) {
+
+        if(target == matrix[r][c])
+            return true;
+
+        else if(matrix[r][c] > target)
+            c--;
+        
+        else 
+            r++;
+    }
+    return false;      
+};
