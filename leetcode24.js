ee192419a7708154e6177076245059b6aa3a37d3
jahidshakil <jahidaladin@gmail.com>
2022-10-06 21:14:16 +0600

    Swap Nodes in Pairs

---
+++ leetcode24.js
@@ -0,0 +1,49 @@
+/*
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)  
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+
+*/
+
+
+// recursive
+
+var swapPairs = function(head) {
+    if(!head || !head.next) return head;
+    var v1 = head, v2 = head.next, v3 = v2.next;
+    v2.next = v1;
+    v1.next = swapPairs(v3);
+    return v2;
+};
+
+
+
+
+//// iterative
+var swapPairs = function(head) {
+    // Store head to return list, Only instance of new data so space is O(1)
+    let dummy = new ListNode(-1);
+    dummy.next = head;
+    
+    let prev = dummy
+    
+    // Traverse list
+    while (head && head.next) {
+        
+    // Declare
+    let n1 = head
+    let n2 = head.next
+        
+        // Swap
+        prev.next = n2
+        n1.next = n2.next
+        n2.next = n1
+        
+        // Assign
+        prev = n1
+        head = n1.next
+    }
+    
+    // The variable prev has been used to create the dummy list. dummy.next is still the head of new list
+    return dummy.next;
+};
